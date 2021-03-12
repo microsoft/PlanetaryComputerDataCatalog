5a1a9699f885b27441afb1a2de867858750d0864
Matt McFarland <mmcfarland@microsoft.com>
2021-03-12 09:56:29 -0500

    Write payload on POST to function
    Allow writing Table storage based on POSTed JSON content.

--- api/AccountSurveyWrite/__init__.py
+++ api/AccountSurveyWrite/__init__.py
@@ -1,28 +1,38 @@
-import uuid
 import json
+import logging
+import uuid
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest, survey: func.Out[str]) -> func.HttpResponse:
 
-    rowKey = str(uuid.uuid4())
+    row_key = str(uuid.uuid4())
+    required_keys = ["name", "email"]
 
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        logging.info("Recieved non-JSON payload")
+        return func.HttpResponse("Unexpected input format", status_code=422)
 
+    valid = all(key in req_body.keys() for key in required_keys)
+
+    if not valid:
+        logging.info("Received payload with missing required keys")
+        return func.HttpResponse(
+            f"Required keys: {', '.join(required_keys)}", status_code=400
+        )
     data = {
-        "Name": name,
+        "Name": req_body.get("name"),
+        "Email": req_body.get("email"),
+        "Industry": req_body.get("industry"),
         "PartitionKey": "survey",
-        "RowKey": rowKey,
+        "RowKey": row_key,
     }
 
+    row_short = row_key[:8]
     survey.set(json.dumps(data))
 
-    return func.HttpResponse(f"Message created with the rowKey: {rowKey[5]}")
+    logging.info(f"Inserted record {row_short}")
+    return func.HttpResponse(f"Message created with the rowKey: {row_short}")
