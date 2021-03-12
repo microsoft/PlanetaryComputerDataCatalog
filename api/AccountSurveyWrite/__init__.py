cc0516058ca07ef0d56fe6fb3e6635e145cc0401
Matt McFarland <mmcfarland@microsoft.com>
2021-03-12 10:30:38 -0500

    Frontend for submitting account request
    Mocks the request to the functional API.

--- api/AccountSurveyWrite/__init__.py
+++ api/AccountSurveyWrite/__init__.py
@@ -1,38 +1,55 @@
 import json
 import logging
+import os
 import uuid
 
+import requests
 import azure.functions as func
+
+from .card import make_card
 
 
 def main(req: func.HttpRequest, survey: func.Out[str]) -> func.HttpResponse:
 
     row_key = str(uuid.uuid4())
     required_keys = ["name", "email"]
+    webhook_url = os.environ.get("NotificationHook")
+    az_env = os.environ.get("AZURE_FUNCTIONS_ENVIRONMENT")
 
     try:
         req_body = req.get_json()
     except ValueError:
-        logging.info("Recieved non-JSON payload")
+        logging.error("Recieved non-JSON payload")
         return func.HttpResponse("Unexpected input format", status_code=422)
 
     valid = all(key in req_body.keys() for key in required_keys)
 
     if not valid:
-        logging.info("Received payload with missing required keys")
+        logging.error("Received payload with missing required keys")
         return func.HttpResponse(
             f"Required keys: {', '.join(required_keys)}", status_code=400
         )
-    data = {
+
+    row_data = {
         "Name": req_body.get("name"),
         "Email": req_body.get("email"),
+        "Affiliation": req_body.get("affiliation"),
         "Industry": req_body.get("industry"),
+        "Note": req_body.get("note"),
+        "Processed": False,
         "PartitionKey": "survey",
         "RowKey": row_key,
     }
 
-    row_short = row_key[:8]
-    survey.set(json.dumps(data))
+    short_key = row_key[:8]
+    survey.set(json.dumps(row_data))
+    logging.debug(f"Inserted record {short_key}")
 
-    logging.info(f"Inserted record {row_short}")
-    return func.HttpResponse(f"Message created with the rowKey: {row_short}")
+    if webhook_url:
+        if az_env != "Development":
+            logging.debug("Sending notification via webhook")
+            requests.post(webhook_url, json=make_card(short_key))
+    else:
+        logging.warning("Notification web hook not configured")
+
+    return func.HttpResponse(status_code=200)
