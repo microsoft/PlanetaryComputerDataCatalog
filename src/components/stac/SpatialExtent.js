6cee22d963094e0eab5ad3818625fcd45970ac85
Matt McFarland <mmcfarland@microsoft.com>
2021-03-19 10:57:18 -0400

    Add leaflet inset extent map
    Replace the attempt at azure maps, as the bundle size was twice that of
    the entire rest of the application. Adds a map to the collection detail
    which shows all spatial extents provided.

--- src/components/stac/SpatialExtent.js
+++ src/components/stac/SpatialExtent.js
@@ -1,68 +1,30 @@
 import React from "react";
-import {
-  AzureMap,
-  AzureMapDataSourceProvider,
-  AzureMapFeature,
-  AzureMapLayerProvider,
-  AzureMapsProvider,
-} from "react-azure-maps";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-import { AuthenticationType } from "azure-maps-control";
-
-const option = {
-  authOptions: {
-    authType: AuthenticationType.subscriptionKey,
-    subscriptionKey: process.env.REACT_APP_AZMAPS_KEY,
-  },
-  showLogo: false,
-  style: "grayscale_light",
-  showFeedbackLink: false,
-};
+import CenteredBounds from "../controls/CenteredBounds";
 
 const SpatialExtent = ({ extent }) => {
   if (!extent) return null;
 
-  const shapes = extent.bbox.map(bb => {
-    const [minX, minY, maxX, maxY] = bb;
-    return [
-      [minX, maxY],
-      [minX, minY],
-      [maxX, minY],
-      [maxX, maxY],
-      [minX, maxY],
-    ];
-  });
-
   return (
-    <AzureMapsProvider>
-      <div
-        style={{ border: "1px solid #ccc", height: "250px", width: "250px" }}
+    <div style={{ width: "300px", height: "200px" }}>
+      <MapContainer
+        center={[-90, 90]}
+        zoom={4}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        style={{ height: "100%" }}
       >
-        <AzureMap options={option}>
-          <AzureMapDataSourceProvider id="extent-datasource">
-            <AzureMapLayerProvider
-              id="spatial-extent"
-              options={{
-                fillOpacity: 0,
-                strokeOpacity: 0.8,
-                strokeColor: "#ff0",
-              }}
-              type={"PolygonLayer"}
-            />
-            {shapes.map((shape, idx) => {
-              return (
-                <AzureMapFeature
-                  key={`shape-${idx}`}
-                  id="collection-extent"
-                  type="Polygon"
-                  coordinates={shape}
-                />
-              );
-            })}
-          </AzureMapDataSourceProvider>
-        </AzureMap>
-      </div>
-    </AzureMapsProvider>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OSM</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          subdomains="abcd"
+          maxZoom={19}
+        />
+        <CenteredBounds extent={extent} />
+      </MapContainer>
+    </div>
   );
 };
 
