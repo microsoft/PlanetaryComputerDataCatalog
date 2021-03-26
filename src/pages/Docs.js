e71e4733befeefff080c50b4da80cd210751defd
Matt McFarland <mmcfarland@microsoft.com>
2021-03-25 21:18:32 -0400

    Add correct page header metadata for Docs

--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Layout from "../components/Layout";
+import SEO from "../components/Seo";
 import RoutedHtml from "../components/docs/RoutedHtml";
 import Topic from "../components/docs/Topic";
 
@@ -36,7 +37,7 @@ const Docs = () => {
             <h2>Documentation</h2>
             <p>
               The Planetary Computer consists of an API layer as well as
-              Computer. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              Compute. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
@@ -50,7 +51,15 @@ const Docs = () => {
     </div>
   );
 
-  return <Layout>{links}</Layout>;
+  return (
+    <Layout>
+      <SEO
+        title="Documentation"
+        description="User guides and reference material for using the Planetary Computer."
+      />
+      {links}
+    </Layout>
+  );
 };
 
 export default Docs;
