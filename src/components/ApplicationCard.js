329fdc24c2d3f4d82c70f2316ade7f7ea0beaf3e
Matt McFarland <mmcfarland@microsoft.com>
2021-04-05 08:18:44 -0400

    Shift focus of applications page to the apps
    Elevate the prominence of the applications over the partners. The AI4E
    Partners page focuses on the partners.

--- src/components/ApplicationCard.js
+++ src/components/ApplicationCard.js
@@ -1,37 +1,24 @@
 import React from "react";
-import { Link, Text } from "@fluentui/react";
-import { FontIcon } from "@fluentui/react/lib/Icon";
-import { mergeStyles } from "@fluentui/react/lib/Styling";
-
-const iconClass = mergeStyles({
-  fontSize: 12,
-  marginTop: 3,
-});
+import { Text } from "@fluentui/react";
+import ChevronLink from "./controls/ChevronLink";
 
 const ApplicationCard = ({ app }) => {
   const { title, thumbnailUrl, description, links } = app;
 
   return (
     <div className="api-datasource-item">
+      <h2 style={{ color: "initial" }}>{title}</h2>
       <img
         alt=""
         src={thumbnailUrl}
         style={{ maxWidth: "100%", objectFit: "contain" }}
       />
-      <h3 style={{ marginBottom: 2, color: "initial" }}>{title}</h3>
       <p style={{ marginBottom: 10, minHeight: 37 }}>{description}</p>
       <Text block variant="medium" style={{ fontWeight: 600 }}>
         {links.map(({ title, url }) => {
           return (
             <div key={title} style={{ paddingBottom: 15 }}>
-              <Link href={url}>
-                {title}
-                <FontIcon
-                  aria-label={title}
-                  iconName="ChevronRightSmall"
-                  className={iconClass}
-                />
-              </Link>
+              <ChevronLink href={url} label={title} />
             </div>
           );
         })}
