46d96c7d9e61cf644c1be80ca3f13d875c709ee6
Matt McFarland <mmcfarland@microsoft.com>
2021-06-30 08:43:57 -0400

    Add breadcrumbs for dataset banner
    Group collections add an interstitial page that's convenient to return to
    via link.

--- src/components/stac/Banner.js
+++ src/components/stac/Banner.js
@@ -1,9 +1,10 @@
-import React from "react";
 import { Text } from "@fluentui/react";
+import { Link } from "react-router-dom";
 
 import Keywords from "./Keywords";
 import { collections as collectionsConfig } from "../../config/datasets.yml";
 import { useHistory } from "react-router";
+import { titleCase } from "utils";
 
 const Banner = ({ collection }) => {
   const history = useHistory();
@@ -17,6 +18,16 @@ const Banner = ({ collection }) => {
     collectionsConfig[collection.id]?.headerImg ||
     collection.assets?.thumbnail?.href;
 
+  const groupId = collection["msft:group_id"];
+  const bannerBreadcrumbs = groupId ? (
+    <>
+      <Link to="/catalog">Datasets</Link> {" > "}
+      <Link to={`/dataset/group/${groupId}`}>{titleCase(groupId)}</Link>
+    </>
+  ) : (
+    <Link to="/catalog">Datasets</Link>
+  );
+
   return (
     <div
       className="page-header"
@@ -27,9 +38,10 @@ const Banner = ({ collection }) => {
       <div className="grid-content">
         <Text
           block
+          className="breadcrumbs"
           styles={{ root: { color: "#fff", fontWeight: 500, marginTop: 5 } }}
         >
-          Datasets
+          {bannerBreadcrumbs}
         </Text>
         <h1 style={{ color: "#fff" }}>{collection.title}</h1>
         <Keywords keywords={collection.keywords} onClick={handleClick} />
