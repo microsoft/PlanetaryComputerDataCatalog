cd424bc42fae6692d6d138257fac78ae295c1efb
Matt McFarland <mmcfarland@microsoft.com>
2021-07-01 06:04:30 -0400

    Allow EPSG codes for cube collections
    Previously, we only had ProjJSON CRS, but TerraClimate added an epsg
    style.

--- src/components/stac/Crs.js
+++ src/components/stac/Crs.js
@@ -1,24 +1,17 @@
-import {
-  FontIcon,
-  HoverCard,
-  HoverCardType,
-  IconButton,
-  mergeStyles,
-  mergeStyleSets,
-} from "@fluentui/react";
-import React from "react";
-import { useCopyToClipboard } from "react-use";
+import NewTabLink from "components/controls/NewTabLink";
 
 import LabeledValue from "../controls/LabeledValue";
 import { useStac } from "./CollectionContext";
+import ProjJsonCrs from "./ProjJsonCrs";
 
 const Crs = () => {
   const collection = useStac();
-  const copyToClipboard = useCopyToClipboard()[1];
 
   // CRS information could be in many different places
   // First attempt is with cube extension dimensions
   const cube = collection["cube:dimensions"];
+  if (!cube) return null;
+
   const crs = cube
     ? Object.values(cube)
         .filter(dimension => {
@@ -28,79 +21,18 @@ const Crs = () => {
         .pop()
     : null;
 
-  if (!crs?.conversion?.method?.name) return null;
+  let crsInfo;
+  if (crs?.conversion?.method?.name) {
+    // ProjJSON
+    crsInfo = <ProjJsonCrs crs={crs} />;
+  } else if (Number.isInteger(crs)) {
+    // EPSG Code
+    crsInfo = (
+      <NewTabLink href={`https://epsg.io/${crs}`}>{`EPSG:${crs}`}</NewTabLink>
+    );
+  }
 
-  const iconClass = mergeStyles({
-    fontSize: 14,
-    margin: 5,
-    position: "absolute",
-  });
-
-  const classNames = mergeStyleSets({
-    compactCard: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100%",
-      padding: "16px 24px",
-      fontWeight: "bold",
-    },
-    expandedCard: {
-      padding: "16px 24px",
-      width: 500,
-    },
-    item: {
-      selectors: {
-        "&:hover": {
-          textDecoration: "underline",
-          cursor: "pointer",
-        },
-      },
-    },
-  });
-
-  const plainCardProps = {
-    onRenderPlainCard: item => {
-      const itemStr = JSON.stringify(item, null, 2);
-      return (
-        <div>
-          <div className={classNames.compactCard}>
-            {item.conversion.method.name}
-          </div>
-          <div className={classNames.expandedCard}>
-            <IconButton
-              iconProps={{ iconName: "Copy" }}
-              onClick={() => copyToClipboard(itemStr)}
-              title="Copy to clipboard"
-              style={{ float: "right" }}
-            />
-            <LabeledValue label="projJSON" />
-            <div className="input_area">
-              <pre>{itemStr}</pre>
-            </div>
-          </div>
-        </div>
-      );
-    },
-    renderData: crs,
-  };
-
-  return (
-    <LabeledValue label="CRS">
-      <HoverCard
-        plainCardProps={plainCardProps}
-        instantOpenOnClick={true}
-        cardDismissDelay={300}
-        cardOpenDelay={300}
-        type={HoverCardType.plain}
-      >
-        <div className={classNames.item}>
-          {crs.conversion.method.name}{" "}
-          <FontIcon iconName="Info" className={iconClass} />
-        </div>
-      </HoverCard>
-    </LabeledValue>
-  );
+  return crsInfo ? <LabeledValue label="CRS">{crsInfo}</LabeledValue> : null;
 };
 
 export default Crs;
