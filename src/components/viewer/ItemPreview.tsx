a2d5242e59870a44ca49ed2cb02cc8f72656e401
Matt McFarland <mmcfarland@microsoft.com>
2021-07-21 15:33:51 -0400

    Preview STAC item results as thumbnails
    Base implementation for rendering thumbnail for the two sources
    possible.

---
+++ src/components/viewer/ItemPreview.tsx
@@ -0,0 +1,36 @@
+import { IStacAsset, IStacItem } from "types/stac";
+
+interface ItemPreviewProps {
+  item: IStacItem;
+}
+
+const thumbnailImageTypes = ["image/png", "image/jpg"];
+
+const assetThumbCheck = (roleKey: string) => {
+  return (asset: IStacAsset): boolean => {
+    return (
+      !!asset?.roles?.includes(roleKey) &&
+      thumbnailImageTypes.includes(asset.type || "")
+    );
+  };
+};
+
+const isThumbnailAsset = assetThumbCheck("thumbnail");
+const isRenderedPreviewAsset = assetThumbCheck("overview");
+
+const ItemPreview = ({ item }: ItemPreviewProps) => {
+  // Get a thumbnail asset, either a absolute url to an image or a preview
+  // endpoint that will generate one on the fly
+  const thumbAsset = Object.values(item.assets).find(
+    asset => isThumbnailAsset(asset) || isRenderedPreviewAsset(asset)
+  );
+
+  if (thumbAsset?.href) {
+    return (
+      <img src={thumbAsset.href} alt={`Rendered thumbnail for item: ${item.id}`} />
+    );
+  }
+  return null;
+};
+
+export default ItemPreview;
