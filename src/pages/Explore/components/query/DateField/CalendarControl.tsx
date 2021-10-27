2324c5ac5a5d2a3e8f6b7c68620fe2234f2a614b
Matt McFarland <mmcfarland@microsoft.com>
2021-10-27 19:13:53 -0400

    validation clean

--- src/pages/Explore/components/query/DateField/CalendarControl.tsx
+++ src/pages/Explore/components/query/DateField/CalendarControl.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useCallback, useContext, useState } from "react";
+import { useEffect, useContext, useState } from "react";
 import { Calendar, MaskedTextField, Stack } from "@fluentui/react";
 import { dayjs, toDateString, toUtcDateString } from "utils";
-import { Dayjs } from "dayjs";
 import { DateFieldContext } from "./context";
 import { DateRangeAction, RangeType } from "./types";
 
@@ -29,11 +28,9 @@ const CalendarControl = ({
 
   const date = workingDates[rangeType];
 
-  const handleSelectDate = (newDate: Date | Dayjs) => {
-    const day = dayjs(newDate);
-    setDateValidation(day.toDate());
-
-    onSelectDate({ [rangeType]: day });
+  const handleSelectDate = (newDate: Date) => {
+    setDateValidation(newDate);
+    onSelectDate({ [rangeType]: dayjs(newDate) });
   };
 
   const handleTextChange = (
@@ -44,76 +41,66 @@ const CalendarControl = ({
     const newDate = newDay.toDate();
 
     // Check valid date format and if it's in range - only set working date if so
-    const valid = newDay.isValid() && setDateValidation(newDay.toDate());
+    const valid = newDay.isValid() && setDateValidation(newDate);
     valid && handleSelectDate(newDate);
   };
 
-  const getErrorMessage = useCallback(
-    (value: Date | string) => {
-      const day = dayjs(value);
-      if (!day.isValid()) return "Invalid date, use MM/DD/YYYY";
+  const getErrorMessage = (value: Date | string) => {
+    const day = dayjs(value);
+    if (!day.isValid()) return "Invalid date, use MM/DD/YYYY";
 
-      if (day.isBefore(validMinDate))
-        return `Date must be after ${toDateString(validMinDate.subtract(1, "day"))}`;
+    if (day.isBefore(validMinDate))
+      return `Date must be after ${toDateString(validMinDate.subtract(1, "day"))}`;
 
-      if (day.isAfter(validMaxDate))
-        return `Date must be before ${toDateString(validMaxDate.add(1, "day"))}`;
+    if (day.isAfter(validMaxDate))
+      return `Date must be before ${toDateString(validMaxDate.add(1, "day"))}`;
 
-      // Validate date ranges to be start <= end
-      if (operator === "between") {
-        if (
-          rangeType === "start" &&
-          workingDates?.end &&
-          day.isAfter(workingDates.end)
-        ) {
-          return `Start date must be before end date (${toDateString(
-            workingDates.end
-          )})`;
-        } else if (
-          rangeType === "end" &&
-          workingDates?.start &&
-          day.isBefore(workingDates.start)
-        ) {
-          return `End date must be after start date (${toDateString(
-            workingDates.start
-          )})`;
-        }
+    // Validate date ranges to be start <= end
+    if (operator === "between") {
+      if (
+        rangeType === "start" &&
+        workingDates?.end &&
+        day.isAfter(workingDates.end)
+      ) {
+        return `Start date must be before end date (${toDateString(
+          workingDates.end
+        )})`;
+      } else if (
+        rangeType === "end" &&
+        workingDates?.start &&
+        day.isBefore(workingDates.start)
+      ) {
+        return `End date must be after start date (${toDateString(
+          workingDates.start
+        )})`;
       }
-      return "";
-    },
-    [
-      operator,
-      rangeType,
-      validMaxDate,
-      validMinDate,
-      workingDates?.end,
-      workingDates?.start,
-    ]
-  );
+    }
+    return "";
+  };
 
-  const setDateValidation = useCallback(
-    (value: Date | string) => {
-      const err = getErrorMessage(value);
-      setErrorMessage(err);
+  const setDateValidation = (value: Date | undefined) => {
+    if (!value) return false;
 
-      // Dispatch to the parent context useing the rangeType key provided
-      const validation = { [rangeType]: !Boolean(err) };
-      if (validation[rangeType] !== validationState[rangeType]) {
-        setValidation(validation);
-      }
+    // The calendar control determines validation state by the presense or absence of a
+    // string which is also used as a validation message.
+    const err = getErrorMessage(value);
+    setErrorMessage(err);
 
-      // The calendar control determines validation state by the presense or absence of a
-      // string which is also used as a validation message.
-      const hasError = Boolean(err);
-      return !hasError;
-    },
-    [getErrorMessage, rangeType, setValidation, validationState]
-  );
+    // Dispatch to the parent context useing the rangeType key provided
+    const validation = { [rangeType]: !Boolean(err) };
+    if (validation[rangeType] !== validationState[rangeType]) {
+      setValidation(validation);
+    }
 
-  // Cross validate date ranges
+    const hasError = Boolean(err);
+    return !hasError;
+  };
+
+  // Cross validate date ranges - when rendering, the current invalid date may
+  // have become valid due to changes in the other date range value.
   useEffect(() => {
     if (errorMessage && operator === "between") {
-      setDateValidation(toDateString(date || ""));
+      setDateValidation(date?.toDate());
     }
   });
 
