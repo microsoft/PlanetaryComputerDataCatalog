196371890f9be8d947e58c26579f85f72609c440
Matt McFarland <mmcfarland@microsoft.com>
2022-05-07 16:30:13 -0400

    Setup component tests for acct required

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,15 +1,8 @@
-import { render } from "@testing-library/react";
+import { render } from "testUtils";
 import { Home } from "./Home";
-import { MemoryRouter, Routes, Route } from "react-router";
 
 test("smoke home render", () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </MemoryRouter>
-  );
+  const { getByText } = render(<Home />);
 
   expect(
     getByText(/Planetary Computer for a Sustainable Future/i)
