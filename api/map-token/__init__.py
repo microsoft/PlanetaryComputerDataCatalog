1b87223a03141d4732d5b2fea61691c0a24a86a0
Manish Kumar Gupta <manishkg@microsoft.com>
2025-07-31 07:53:08 -0700

    use sharedkey mechanism for authenticating to AzMaps (#507)
    * use sharedkey mechanism for authenticating to AzMaps
    
    * Add Azure Maps subscription key configuration to environment files
    
    * Add escape key functionality to close import dialog in explorer tests

--- api/map-token/__init__.py
+++ api/map-token/__init__.py
@@ -25,14 +25,17 @@ class TokenResponse(TypedDict):
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logger.debug("Python HTTP trigger function processed a request.")
-    try:
-        logger.debug("Getting azure maps token")
-        token = credential.get_token("https://atlas.microsoft.com/.default")
-        logger.debug("Token acquired")
+    # Comment out the implementation below and fail this call forcibly
+    return func.HttpResponse("This API is not implemented yet", status_code=501)
+    # Uncomment the implementation below to enable token retrieval
+    # try:
+    #     logger.debug("Getting azure maps token")
+    #     token = credential.get_token("https://atlas.microsoft.com/.default")
+    #     logger.debug("Token acquired")
 
-        resp: TokenResponse = {"token": token.token, "expires_on": token.expires_on}
+    #     resp: TokenResponse = {"token": token.token, "expires_on": token.expires_on}
 
-        return func.HttpResponse(status_code=200, body=json.dumps(resp))
-    except ClientAuthenticationError:
-        logger.exception(f"Error getting azure maps token")
-        return func.HttpResponse("Error getting token", status_code=500)
+    #     return func.HttpResponse(status_code=200, body=json.dumps(resp))
+    # except ClientAuthenticationError:
+    #     logger.exception(f"Error getting azure maps token")
+    #     return func.HttpResponse("Error getting token", status_code=500)
