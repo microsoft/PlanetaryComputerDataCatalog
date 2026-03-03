a3b8651eaa5f28cd2a4174e0918ee75a603b2017
Pete Gadomski <pete.gadomski@gmail.com>
2026-03-03 13:43:29 -0700

    fix: join

--- src/utils/stac.js
+++ src/utils/stac.js
@@ -683,7 +683,7 @@ export const renderItemColumn = (item, _, column) => {
       if (Array.isArray(fieldContent)) {
         fieldContent = fieldContent.join(", ");
       } else if (isObject(fieldContent)) {
-        <Revealer>{", ".join(Object.keys(fieldContent))}</Revealer>;
+        <Revealer>{Object.keys(fieldContent).join(", ")}</Revealer>;
       }
       return stacFormatter.format(fieldContent, column.fieldName);
   }
